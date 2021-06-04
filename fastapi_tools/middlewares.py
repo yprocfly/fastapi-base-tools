4c73166de06eb5ded1e84ec12a981fe47252453e
yangpeng <peng.yang@lizhiweike.com>
2021-06-04 14:53:19 +0800

    fix-bug

--- fastapi_tools/middlewares.py
+++ fastapi_tools/middlewares.py
@@ -1,3 +1,4 @@
+import functools
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.types import ASGIApp, Receive, Scope, Send, Message
@@ -7,19 +8,35 @@ class SimpleBaseMiddleware:
 
     def __init__(self, app: ASGIApp) -> None:
         self.app = app
-        self.default_send = None    # type Send
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] != "http":
             await self.app(scope, receive, send)
             return
 
-        self.default_send = send
         request = Request(scope, receive=receive)
+        send = functools.partial(self.send, send=send, request=request)
 
         response = await self.before_request(request) or self.app
-        await response(request.scope, request.receive, self.send)
+        await response(request.scope, request.receive, send)
         await self.after_request(request)
+
+    async def get_receive(self, request):
+        """获取请求receive"""
+        message = await request.receive()
+
+        async def _receive():
+            return message
+
+        request._receive = _receive
+        return message
+
+    async def get_body(self, request):
+        """获取请求BODY"""
+        message = await self.get_receive(request)
+        if message['type'] == 'http.request':
+            return message.get('body', b"")
+        return b""
 
     async def before_request(self, request: Request) -> [Response, None]:
         """如果需要修改请求信息，可直接重写此方法"""
@@ -29,9 +46,9 @@ class SimpleBaseMiddleware:
         """请求后的处理【记录请求耗时等，注意这里没办法对响应结果进行处理】"""
         return None
 
-    async def send(self, message: Message) -> None:
+    async def send(self, message: Message, send: Send, request: Request) -> None:
         """重写send方法【不重写则默认使用原来的】"""
-        await self.default_send(message)
+        await send(message)
 
     async def update_request_header(self, request, key, value):
         """更新请求头"""
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read_requirements(filename):
 
 setuptools.setup(
     name="fastapi_base_tools",
-    version="0.0.1",
+    version="0.0.2",
     author="Yang Peng",
     author_email="yp_rocfly@foxmail.com",
     description="some fastapi base tools",
